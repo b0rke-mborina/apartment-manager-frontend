8588bb0965bbe518affc470f46ae6f50a4bc2cc9
b0rke-mborina <mborina@net.hr>
2022-05-31 13:15:22 +0200

    Improvements for guest and reservation items

--- src/components/GuestItem.vue
+++ src/components/GuestItem.vue
@@ -1,17 +1,37 @@
 <template>
-	<v-card>
-		<v-icon>mdi-account</v-icon>
-		<span>{{ guest.firstName }} {{ guest.lastName }}</span>
-		<br>
-		<span>{{ guest.city }}, {{ guest.country }}</span>
-		<br>
-		<span>{{ guest.period.startDate }} - {{ guest.period.endDate }}</span>
+	<v-card color="#E3EAEF" class="my-3 pa-4 rounded-xl main-grid">
+		<div class="grid-div">
+			<div class="name-location">
+				<v-icon class="mx-2">mdi-account</v-icon>
+				<span class="mx-2">{{ guest.firstName }} {{ guest.lastName }}</span>
+				<span class="mx-2 location">{{ guest.city }}, {{ guest.country }}</span>
+			</div>
+			<div class="period-dates">
+				<span class="mx-2">{{ guest.period.startDate }} - {{ guest.period.endDate }}</span>
+				<v-chip color="green" class="mx-2">CURRENT GUEST</v-chip><!-- {{ guest }} -->
+			</div>
+		</div>
+		<div class="edit-delete-icons">
+			<v-icon icon color="#0000FF" class="mx-2">mdi-pencil</v-icon>
+			<v-icon icon color="#FF0000" class="mx-2">mdi-trash-can-outline</v-icon>
+		</div>
 	</v-card>
 </template>
 
 <script>
 export default {
 	name: 'GuestItem',
+	data() {
+		return {
+			items: [
+				'CURRENT GUEST',
+				'FORMER GUEST',
+				'FUTURE GUEST',
+				'POSSIBLE GUEST',
+				'POTENTIAL GUEST'
+			],
+		}
+	},
 	props: {
 		guest: Object
 	}
@@ -19,5 +39,35 @@ export default {
 </script>
 
 <style scoped>
-
+.main-grid {
+	display: grid;
+	grid-template-columns: 6fr 1fr;
+	font-size: 16px;
+}
+.grid-div {
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+}
+.location {
+	font-size: 12px;
+}
+.name-location, .period-dates {
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+	align-items: center;
+	padding: 10px 0px;
+}
+.edit-delete-icons {
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+	align-items: right;
+	padding: 10px 0px;
+}
+@media (max-width:500px) {
+	.grid-div {
+		grid-template-columns: auto;
+	}
+}
 </style>
--- src/components/ReservationItem.vue
+++ src/components/ReservationItem.vue
@@ -1,11 +1,28 @@
 <template>
-	<v-card>
-		<v-icon>mdi-calendar-check</v-icon>
-		<span>{{ reservation.currentState }}, {{ reservation.price.value }} {{ reservation.price.currency }}</span>
-		<br>
-		<span>{{ reservation.madeByGuest.email }}, {{ reservation.madeByGuest.phoneNumber }}</span>
-		<br>
-		<span>{{ reservation.period.startDate }} - {{ reservation.period.endDate }}</span>
+	<v-card color="#E3EAEF" class="my-3 pa-4 rounded-xl main-grid">
+		<div class="grid-div">
+			<div class="reservation-info">
+				<v-icon class="mx-2">mdi-calendar-check</v-icon>
+				<span v-if="reservation.madeByGuest.email && reservation.madeByGuest.phoneNumber" class="mx-2 text-center">
+					{{ reservation.madeByGuest.email }}
+				</span>
+				<span v-if="reservation.madeByGuest.email && reservation.madeByGuest.phoneNumber" class="mx-2 text-center">
+					{{ reservation.madeByGuest.phoneNumber }}
+				</span>
+				<span v-else class="mx-2 text-center">
+					{{ reservation.madeByGuest.firstName }} {{ reservation.madeByGuest.lastName }}
+				</span>
+			</div>
+			<div class="money">
+				<span class="mx-2">{{ reservation.price.value }} {{ reservation.price.currency }}</span>
+				<v-chip color="green" class="mx-2">{{ reservation.currentState }}</v-chip>
+			</div>
+		</div>
+
+		<div class="edit-delete-icons">
+			<v-icon icon color="#0000FF" class="mx-2">mdi-pencil</v-icon>
+			<v-icon icon color="#FF0000" class="mx-2">mdi-trash-can-outline</v-icon>
+		</div>
 	</v-card>
 </template>
 
@@ -19,5 +36,32 @@ export default {
 </script>
 
 <style scoped>
-
+.main-grid {
+	display: grid;
+	grid-template-columns: 6fr 1fr;
+	font-size: 16px;
+}
+.grid-div {
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+}
+.reservation-info, .money {
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+	align-items: center;
+	padding: 10px 0px;
+}
+.edit-delete-icons {
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+	align-items: right;
+	padding: 10px 0px;
+}
+@media (max-width:450px) {
+	.grid-div {
+		grid-template-columns: auto;
+	}
+}
 </style>
